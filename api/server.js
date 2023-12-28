b66394ddd661f20d92152bd5b005bbb2aa4711f2
AksharaAK <akshann1606@gmail.com>
2023-12-28 14:08:02 +0530

    remove unused package

--- api/server.js
+++ api/server.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const cors = require("cors");
 require("dotenv").config();
 const { format } = require("date-fns");
 
@@ -17,12 +16,10 @@ const auth = {
   },
 };
 console.log("hello world");
-
 io.on("connection", (socket) => {
   console.log("user is connected");
   socket.on("message", async (message, messageContext) => {
     const completionsURL = "https://chat.nbox.ai/api/chat/completions/";
-
     const body = JSON.stringify({
       temperature: 0.5,
       messages: [
@@ -34,12 +31,9 @@ io.on("connection", (socket) => {
       stream: false,
       max_tokens: 1000,
     });
-
     try {
       const response = await axios.post(completionsURL, body, auth);
-
       const chatNBXResponseObj = response.data.choices[0].message;
-
       const latestMessage = [
         ...messageContext,
         { role: "user", content: message },
@@ -48,13 +42,11 @@ io.on("connection", (socket) => {
           content: chatNBXResponseObj.content,
         },
       ];
-
       const latestMeta = {
         role: chatNBXResponseObj.role,
         content: chatNBXResponseObj.content,
         created: format(new Date(), "p"),
       };
-
       io.emit("response", latestMessage, latestMeta);
     } catch (error) {
       console.error("Error calling ChatNBX API:", error);
