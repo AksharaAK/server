ee31246951ba387b40b125e026cf860066eef901
AksharaAK <akshann1606@gmail.com>
2023-12-28 12:57:34 +0530

    setup socket

--- api/server.js
+++ api/server.js
@@ -1,19 +1,13 @@
 const axios = require("axios");
-const http = require("http");
 require("dotenv").config();
 const { format } = require("date-fns");
-const { Server } = require("socket.io");
 
-const httpServer = http.createServer();
-
-const io = new Server(httpServer, {
+const io = require("socket.io")(3000, {
   cors: {
-    origin: "https://client-seven-taupe.vercel.app",
+    origin: "https://client-seven-taupe.vercel.app/",
     methods: ["GET", "POST"],
   },
-  transports: ["websocket"],
 });
-
 const auth = {
   headers: {
     "Content-Type": "application/json",
