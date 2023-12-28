48fd767ab896b94b99479e531f6fe36ac54f3ba2
AksharaAK <akshann1606@gmail.com>
2023-12-28 11:25:17 +0530

    test locally

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { format } = require("date-fns");
 
 const io = require("socket.io")(3000, {
   cors: {
-    origin: "https://client-seven-taupe.vercel.app/",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
@@ -15,51 +15,48 @@ const auth = {
     Authorization: process.env.API_KEY,
   },
 };
+console.log("hello world");
 
-exports.handler = async (event, context) => {
-  io.on("connection", (socket) => {
-    console.log("user is connected");
-    socket.on("message", async (message, messageContext) => {
-      const completionsURL = "https://chat.nbox.ai/api/chat/completions/";
+io.on("connection", (socket) => {
+  console.log("user is connected");
+  socket.on("message", async (message, messageContext) => {
+    const completionsURL = "https://chat.nbox.ai/api/chat/completions/";
 
-      const body = JSON.stringify({
-        temperature: 0.5,
-        messages: [
-          { role: "system", content: "You are ChatNBX" },
-          ...messageContext,
-          { role: "user", content: message },
-        ],
-        model: "nous-hermes-13b-4k",
-        stream: false,
-        max_tokens: 1000,
-      });
+    const body = JSON.stringify({
+      temperature: 0.5,
+      messages: [
+        { role: "system", content: "You are ChatNBX" },
+        ...messageContext,
+        { role: "user", content: message },
+      ],
+      model: "nous-hermes-13b-4k",
+      stream: false,
+      max_tokens: 1000,
+    });
 
-      try {
-        const response = await axios.post(completionsURL, body, auth);
+    try {
+      const response = await axios.post(completionsURL, body, auth);
 
-        const chatNBXResponseObj = response.data.choices[0].message;
+      const chatNBXResponseObj = response.data.choices[0].message;
 
-        const latestMessage = [
-          ...messageContext,
-          { role: "user", content: message },
-          {
-            role: chatNBXResponseObj.role,
-            content: chatNBXResponseObj.content,
-          },
-        ];
-
-        const latestMeta = {
+      const latestMessage = [
+        ...messageContext,
+        { role: "user", content: message },
+        {
           role: chatNBXResponseObj.role,
           content: chatNBXResponseObj.content,
-          created: format(new Date(), "p"),
-        };
+        },
+      ];
 
-        io.emit("response", latestMessage, latestMeta);
-      } catch (error) {
-        console.error("Error calling ChatNBX API:", error);
-      }
-    });
+      const latestMeta = {
+        role: chatNBXResponseObj.role,
+        content: chatNBXResponseObj.content,
+        created: format(new Date(), "p"),
+      };
+
+      io.emit("response", latestMessage, latestMeta);
+    } catch (error) {
+      console.error("Error calling ChatNBX API:", error);
+    }
   });
-
-  console.log("hello world");
-};
+});
